fb449d571e1d1817a03692bcc40a17ea79e622af
stigmataA <r.muhutdinov@gmail.com>
2024-10-04 11:04:24 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -7,17 +7,17 @@ def personal_sum(numbers):
         except TypeError:  # исключение на некорректные данные
             incorrect_data += 1  # суммирование не корректных данных
             print(f'Некорректный тип данных для подсчета суммы - {i}')  # вывод ошибки
-    return (result, incorrect_data)  # возврат результата суммы все чисел, количество некорректных данных
+    return (result, incorrect_data)  # возврат результата суммы всех чисел, количество некорректных данных
 
 def calculate_average(numbers):
     try:
-        per_sum = personal_sum(numbers)
-        avg = per_sum[0] / (len(numbers) - per_sum[1])
-        return avg  # исключение (деление на 0)
-    except ZeroDivisionError:
+        per_sum = personal_sum(numbers) # обращение к функции personal_sum
+        average_sum = per_sum[0] / (len(numbers) - per_sum[1]) # вычисление среднегоарифметического
+        return average_sum
+    except ZeroDivisionError: # исключение (деление на 0)
         return 0
     except TypeError:  # исключение на некорректные данные
-        print('В numbers записан некорректный тип данных')
+        print('В numbers записан некорректный тип данных') #вывод ошибки
         return None
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
