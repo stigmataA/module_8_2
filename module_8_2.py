dcd11590de5e34cfb0b6e3312294618292cbdf2f
stigmataA <r.muhutdinov@gmail.com>
2024-10-04 10:37:43 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -1,35 +1,25 @@
-
 def personal_sum(*numbers):
-    """среднее арифметическое всех чисел"""
-    result = 0 # сумма всех чисел (первоначально равно 0)
-    incorrect_data = 0 # количество некорректных данных (первоначально равно 0)
-    for i in numbers: # перебор всех чисел в numbers
-
-        for j in i:
-
-            try:
-                result += j # суммирование в результат
-            except TypeError: # исключение на некорректные данные
-                incorrect_data += 1 # суммирование не корректных данных
-                print(f'некорректный тип данных для подсчета суммы - {j}') # вывод ошибки
-    return result, incorrect_data # возврат результата суммы все чисел, количество некорректных данных
-
+    result = 0  # сумма всех чисел (первоначально равно 0)
+    incorrect_data = 0  # количество некорректных данных (первоначально равно 0)
+    for i in numbers:  # перебор всех чисел в numbers
+        try:
+            result += i  # суммирование в результат
+        except TypeError:  # исключение на некорректные данные
+            incorrect_data += 1  # суммирование не корректных данных
+            print(f'Некорректный тип данных для подсчета суммы - {i}')  # вывод ошибки
+    return (result, incorrect_data)  # возврат результата суммы все чисел, количество некорректных данных
 
 def calculate_average(*numbers):
-    """Среднее арифметическое - сумма всех данных делённая на их количество"""
-    if isinstance(*numbers, int): # проверка на корректные данные (numbers - целое число)
-        return None
     try:
-        average_sum = personal_sum(*numbers)
-
-        return average_sum[0] / (len(*numbers) - average_sum[1])
-    except ZeroDivisionError: # исключение (деление на 0)
+        per_sum = personal_sum(numbers)
+        avg = per_sum[0] / (len(numbers) - per_sum[1])
+        return avg  # исключение (деление на 0)
+    except ZeroDivisionError:
         return 0
-    except TypeError: # исключение на некорректные данные
-        return (f'В numbers записан некорректный тип данных') #
+    except TypeError:  # исключение на некорректные данные
+        print('В numbers записан некорректный тип данных')
+        return None
 
-
-# Вывод результата
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
 print(f'Результат 3: {calculate_average(567)}')
